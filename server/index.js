7fdeabd4c47774c8143e730858b25e6ad80e706a
Aizam Jigjidsuren <aizamjig@gmail.com>
2020-06-11 11:07:05 -0700

    Write socket connection methods for the admin when new users join

--- server/index.js
+++ server/index.js
@@ -12,12 +12,20 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection');
-
     socket.on('join', ({ name, room }, callback) => {
-        console.log(name, room)
+        const { error, user } = addUser({ id: socket.id, name, room });
 
+        if (error) return callback(error);
+        //Admin emitted message from the backend
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!`});
+
+        socket.join(user.room);
+
+        callback();
     })
+    //User emitted sendMessage will be on the client side
+    socket.on('sendMessage')
     socket.on('disconnect', () => {
         console.log('User left');
     });
