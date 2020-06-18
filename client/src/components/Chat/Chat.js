9a16e64c24801e7a0ada25f75dcf34b5e6540786
Aizam Jigjidsuren <aizamjig@gmail.com>
2020-06-17 20:38:04 -0700

    Add online users box that displays the users that are active in the chat in real-time.

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -5,6 +5,8 @@ import io from 'socket.io-client';
 import './Chat.css'
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
+import TextContainer from '../TextContainer/TextContainer';
 
 let socket;
 
@@ -14,6 +16,8 @@ const Chat = ({ location }) => {
     const [room, setRoom] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
+    const [users, setUsers] = useState('');
+
     const ENDPOINT = 'localhost:5000';
 
     //JOIN
@@ -24,22 +28,28 @@ const Chat = ({ location }) => {
         setName(name);
         setRoom(room);
 
-        socket.emit('join', { name, room }, () => {
-            
+        socket.emit('join', { name, room }, (error) => {
+            if (error) {
+                alert(error);
+            }
+ 
         });
 
-        return () => {
-            socket.emit('disconnect');
-            socket.off();
-        }
+        // return () => {
+        //     socket.emit('disconnect');
+        //     socket.off();
+        // }
     }, [ENDPOINT, location.search]);
 
     //Handle admin message
     useEffect(() => {
         socket.on('message', (message) => {
-            setMessages([...messages, message])
-        })
-    }, [messages]);
+            setMessages(messages => [...messages, message])
+        });
+        socket.on('roomData', ({ users }) => {
+            setUsers(users);
+        });
+    }, []);
 
     //SEND MESSAGES
 
@@ -55,8 +65,10 @@ const Chat = ({ location }) => {
         <div className='outerContainer'>
             <div className='container'>
                 <InfoBar room={room}/>
-                <Input/>
+                <Messages messages={messages} name={name}/>
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
             </div>
+            <TextContainer users={users}/>
         </div> 
     )
 }
--- server/index.js
+++ server/index.js
@@ -16,11 +16,12 @@ io.on('connection', (socket) => {
         const { error, user } = addUser({ id: socket.id, name, room });
 
         if (error) return callback(error);
+        socket.join(user.room);
         //Admin emitted message from the backend to the client
         socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
         socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!`});
 
-        socket.join(user.room);
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) })
 
         callback();
     })
@@ -33,7 +34,12 @@ io.on('connection', (socket) => {
         callback();
     })
     socket.on('disconnect', () => {
-        console.log('User left');
+        const user = removeUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.`});
+            io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+        }
     });
 });
 
